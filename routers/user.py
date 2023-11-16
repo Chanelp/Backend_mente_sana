9370c08ee61c63dd7c37d4f8f6ca036a0b87b5ed
chanelp <mariannissanchez27@gmail.com>
2023-11-16 19:51:16 -0400

    Router de usuarios

--- routers/user.py
+++ routers/user.py
@@ -1,5 +1,4 @@
-from fastapi import APIRouter
-from fastapi import HTTPException
+from fastapi import APIRouter, HTTPException
 from fastapi.responses import JSONResponse
 from schemas.user import User
 from services.user import UserService
@@ -7,14 +6,14 @@ from config.database import Session
 
 user_router = APIRouter()
 
-@user_router.post(path="/users", tags= ["Users"], response_model= dict, status_code= 201)
+@user_router.post(path= "/users", tags= ["Users"], response_model= dict, status_code= 201)
 async def create_user(new_user: User) -> dict:
     try:
         db = Session()
     except HTTPException as e:
-        raise HTTPException(status_code=500, detail=str(e))
+        raise HTTPException(status_code = 500, detail = str(e))
     
     else:
         UserService(db).register_user(new_user)
-        return JSONResponse(content= {"message": "User sucessfully registered!"}, status_code= 201)
+        return JSONResponse(content= {"message": "Usuario registrado exitosamente!"}, status_code= 201)
 
