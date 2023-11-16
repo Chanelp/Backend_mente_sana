9b682b37d1235ffd48467b6e820c5e07851f436f
chanelp <mariannissanchez27@gmail.com>
2023-11-16 19:49:42 -0400

    Carpeta de middlewares

--- models/user.py
+++ models/user.py
@@ -1,11 +1,11 @@
+from datetime import datetime
 from config.database import Base
 from sqlalchemy import Column, String, Integer, DateTime, Date
-from datetime import datetime
 
 class UserModel(Base):
     __tablename__ = "users"
     
-    id = Column(Integer, primary_key= True, autoincrement= True)
+    id = Column(Integer, primary_key = True, autoincrement = True)
     name = Column(String)
     email = Column(String)
     password = Column(String)
