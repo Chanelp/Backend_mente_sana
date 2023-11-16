a9e4e1733a2b811dc0cafce80772343ccd246a6f
chanelp <mariannissanchez27@gmail.com>
2023-11-16 19:52:13 -0400

    Schema de usuarios

--- schemas/user.py
+++ schemas/user.py
@@ -2,6 +2,7 @@ from pydantic import BaseModel
 from typing import Optional
 from datetime import datetime, date
 
+
 class User(BaseModel):
     id: Optional[int] = None
     name: str
@@ -13,13 +14,13 @@ class User(BaseModel):
     rol: str = "User"
 
     model_config = {
-        "json_schema_extra":{
+        "json_schema_extra": {
             "examples": [{
-                    "name": "Nombre del usuario",
-                    "email": "Correo electrónico",
-                    "password": "Contraseña",
-                    "date_birth": "Fecha nacimiento 0000-00-00",
-                    "genre": "Género"
+                "name": "Nombre del usuario",
+                "email": "Correo electrónico",
+                "password": "Contraseña",
+                "date_birth": "Fecha nacimiento 0000-00-00",
+                "genre": "Género"
             }]
         }
     }
