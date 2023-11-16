d3b9e7b95953337ede7a24e259f2878e79ff5c7e
chanelp <mariannissanchez27@gmail.com>
2023-11-16 19:49:59 -0400

    Modelo de usuarios

--- models/user.py
+++ models/user.py
@@ -3,6 +3,7 @@ from config.database import Base
 from sqlalchemy import Column, String, Integer, DateTime, Date
 
 class UserModel(Base):
+    
     __tablename__ = "users"
     
     id = Column(Integer, primary_key = True, autoincrement = True)
