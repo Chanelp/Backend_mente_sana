22449d71bca2c0ab09f49f2908897ecb2c22b2d3
chanelp <mariannissanchez27@gmail.com>
2023-11-24 21:29:32 -0400

    Configuración para desplegar en Railway

--- main.py
+++ main.py
@@ -8,3 +8,7 @@ app.title = "API para la plataforma de salud mental en línea."
 app.include_router(user_router)
 
 Base.metadata.create_all(bind = engine)
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="0.0.0.0",
+                port=int(os.environ.get("PORT", 8000)))
