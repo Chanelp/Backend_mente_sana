e078617029060fe96ab0c022cf97160a40d5806e
Raynier Perez Minyety <rayniercraft@gmail.com>
2023-12-11 13:25:11 -0400

    General changes

--- models/statuses.py
+++ models/statuses.py
@@ -10,15 +10,35 @@ class StatusesModel(Base):
 
     createAt = createAt.copy()
 
-    def __init__(self, status):
+    def __init__(self, status, **kwargs):
         self.status = status
+        if (kwargs["id"] != None): self.id = kwargs['id']
+
 
 
     @classmethod
     def create_default_records(self):
+<<<<<<< Updated upstream
         db = Session()
         if not db.query(self).first():
             db.add(StatusesModel("Disponible"))
             db.add(StatusesModel("En espera"))
             db.add(StatusesModel("No disponible"))
             db.commit()
+=======
+        try:
+            db = Session()
+            DEFAULT_STATUSES = [
+                ("Disponible", 1), # ID= 1
+                ("En espera", 2), # ID= 2
+                ("No disponible", 3), # ID= 3
+            ]
+            if db.query(self).count() != len(DEFAULT_STATUSES):
+                from sqlalchemy.sql import text as sa_text
+                db.execute(sa_text('''DELETE FROM statuses'''))
+                for defaulValue in DEFAULT_STATUSES:
+                    db.add(StatusesModel(defaulValue[0], id = defaulValue[1])) 
+                db.commit()
+        except:
+            print('=== ERROR AL CREAR ESTADOS ===')
+>>>>>>> Stashed changes
